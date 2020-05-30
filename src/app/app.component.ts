40ce668f10b7a6911a534723a109ba70dd2092fc
Dominika Zając <dominikaz@qualtrics.com>
2020-05-30 21:22:46 +0200

    replace img paths for stackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  weather = 'normal'
+  pogoda = "normalnie"
+  wybranyObrazek = "https://github.com/dominika-zajac/if-example/blob/master/src/assets/base.png?raw=true"
 
   setWeather(newWeather: string) {
-    this.weather = newWeather;
+    this.pogoda = newWeather;
+    this.wybierzObrazek();
+  }
+
+  wybierzObrazek() {
+    if(this.pogoda === "słonecznie") {
+      this.załóżOkularyPrzeciwsłoneczne()
+    }
+    if(this.pogoda === "deszczowo") {
+      this.zabierzParasol()
+    } 
+    if (this.pogoda === "normalnie") {
+      this.nicNieZabieraj()
+    }
+  }
+
+
+  załóżOkularyPrzeciwsłoneczne() {
+    this.wybranyObrazek = "https://github.com/dominika-zajac/if-example/blob/master/src/assets/sunny.png?raw=true"
+  }
+
+  zabierzParasol() {
+    this.wybranyObrazek = "https://github.com/dominika-zajac/if-example/blob/master/src/assets/rainy.png?raw=true"
+  }
+
+  nicNieZabieraj() {
+    this.wybranyObrazek = "https://github.com/dominika-zajac/if-example/blob/master/src/assets/base.png?raw=true"
   }
 }
